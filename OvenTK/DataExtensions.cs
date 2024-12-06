a3dd6e2f4e477c8cb1546b6101318c342d0e3ef7
Tec <daniel112092@gmail.com>
2024-12-06 20:36:26 +0100

    Add project files.

---
+++ OvenTK/DataExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace OvenTK.OvenTK;
+
+public static class DataExtensions
+{
+    public static int SizeOf<T>(this ReadOnlySpan<T> arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this Span<T> arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this ReadOnlyMemory<T> arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this Memory<T> arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this T[,,] arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this T[,] arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this T[] arr) => arr.Length * Unsafe.SizeOf<T>();
+
+    public static int SizeOf<T>(this T _) where T : struct => Unsafe.SizeOf<T>();
+}
