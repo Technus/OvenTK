ca9f9dcb87dacdb60048548c3964ee27a2e5b380
Tec <daniel112092@gmail.com>
2024-12-06 20:36:31 +0100

    nice

--- TestApp/MainViewModel.cs
+++ TestApp/MainViewModel.cs
@@ -1,4 +1,4 @@
-﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Graphics.OpenGL4;
 using OvenTK.Lib;
 using System.Drawing;
 using System.Runtime.InteropServices;
@@ -121,61 +121,11 @@ public class MainViewModel : DependencyObject
             VertexArrayAttrib.Create(_buffers[4], 4, VertexAttribType.Float, sizeof(float)*4, false, 1),
         ]);
 
-        _texture = Texture.CreateFrom(Properties.Resources.tower1);
+        _texture = Texture.CreateFrom(
+            Application.GetResourceStream(new Uri(@"\Resources\tower1.png", UriKind.Relative)).Stream);
 
-        _shader = Shader.LoadFromText(
-          $$"""
-      #version 460 compatibility
-      //Each group (in,out, uniform, uniform sampler2d, etc.) has own location layout
-
-      layout (location = 0) in vec2 aVertice; //Attribs
-      layout (location = 1) in vec4 aInstancePosition;
-      layout (location = 2) in vec4 aData;
-
-      layout (location = 0) out vec2 texPos;//to next shader
-      layout (location = 1) out vec4 color;//to next shader
-
-      layout (binding = 0) uniform Uniform {
-          vec3 eggNog;
-          mat4 projection;
-          mat4 view;
-      };
-
-      void main()
-      {
-          //the gl_Position (clip-space output position) here must fall between [-1,1],[-1,1],[-1,1],[for normalization, usually: 1]
-          gl_Position = vec4(aVertice, 0, 1.0);
-          gl_Position.x /= 25;
-          gl_Position.y /= 25;
-          gl_Position.x += aInstancePosition.x;
-          gl_Position.y += aInstancePosition.y;
-          //gl_Position = projection * view * vec4(aVertice.x/25 + aInstancePosition.x, aVertice.y/25 + aInstancePosition.y, 0, 1.0);
-          texPos.xy = gl_Position.xy;
-          color = aData;
-      }
-      """,
-          $$"""
-      #version 460 compatibility
-      
-      layout (location = 0) in vec2 texPos;//from shader
-      layout (location = 1) in vec4 color;//to next shader
-
-      layout (binding = 0) uniform Uniform {
-          vec3 eggNog;
-          mat4 projection;
-          mat4 view;
-      };
-
-      //samplers sample in range: [0,1],[0,1]
-      layout(binding = 0) uniform sampler2D diffuseTex;//binds to texture unit
-
-      void main()
-      {
-          //vec4 colorzz = texture(diffuseTex, (texPos + 0.5) / 1.0);
-          //gl_FragColor = colorzz;
-          //gl_FragColor = vec4(eggNog.rgb, 1.0);
-          gl_FragColor = color;
-      }
-      """);
+        _shader = Shader.CreateFromAsync(
+            Application.GetResourceStream(new Uri(@"\Resources\vertex.glsl", UriKind.Relative)).Stream,
+            Application.GetResourceStream(new Uri(@"\Resources\fragment.glsl", UriKind.Relative)).Stream).Result;
     }
 }
---
+++ TestApp/Resources/vertex.glsl
@@ -0,0 +1,28 @@
+﻿#version 460 compatibility
+//Each group (in,out, uniform, uniform sampler2d, etc.) has own location layout
+
+layout (location = 0) in vec2 aVertice; //Attribs
+layout (location = 1) in vec4 aInstancePosition;
+layout (location = 2) in vec4 aData;
+
+layout (location = 0) out vec2 texPos; //to next shader
+layout (location = 1) out vec4 color; //to next shader
+
+layout (binding = 0) uniform Uniform {
+    vec3 eggNog;
+    mat4 projection;
+    mat4 view;
+};
+
+void main()
+{
+    //the gl_Position (clip-space output position) here must fall between [-1,1],[-1,1],[-1,1],[for normalization, usually: 1]
+    gl_Position = vec4(aVertice, 0, 1.0);
+    gl_Position.x /= 25;
+    gl_Position.y /= 25;
+    gl_Position.x += aInstancePosition.x;
+    gl_Position.y += aInstancePosition.y;
+    //gl_Position = projection * view * vec4(aVertice.x/25 + aInstancePosition.x, aVertice.y/25 + aInstancePosition.y, 0, 1.0);
+    texPos.xy = gl_Position.xy;
+    color = aData;
+}
